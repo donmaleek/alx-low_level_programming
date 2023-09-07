e303d64dec9cb4bcfa271919ce63de37779aaddd
donmaleek <bamboloshari@gmail.com>
2023-09-07 13:29:04 -0700

    qn 6 of beginner level c programs

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the given phrase
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, otherwise non zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
