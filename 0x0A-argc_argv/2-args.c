4376bb3c2913797897966f9c6672ae9896a7c628
donmaleek <bamboloshari@gmail.com>
2023-10-02 12:44:56 -0700

    qn2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+  
+/**
+ * main - this program prints the auguments given
+ *
+ *@argc: This is augment counter
+ *@argv: This is augment vector
+ *
+ *Return: always return zero
+ */
+ int main(int argc, char *argv[])
+ 	{
+	int i;
+
+        /*printf("%d\n", argc - 1);*/
+        /*printf("lets see whats going on on argv\n");*/
+        for (i = 0; i < argc; i++)
+        {
+        printf("%s\n", argv[i]);
+        }
+        return (0);
+	}
+
+
