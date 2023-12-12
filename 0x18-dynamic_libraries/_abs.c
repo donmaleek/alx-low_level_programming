b075659df28e5ddbe30afa7f5f4a02665bbd40e0
donmaleek <bamboloshari@gmail.com>
2023-12-11 16:30:41 -0800

    update

---
+++ 0x18-dynamic_libraries/_abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * main -The start of the program
+ *
+ * Return: zero if successfull
+ */
+
+int main(void)
+{
+	int _abs(void);
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/_atoi.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+ #include "main.h"
+ /*
+  * main - the start of the program
+  *
+  * Return: zero if successfull
+  */
+
+int main(void)
+{
+	int _atoi(void);
+	return (0);
+}
