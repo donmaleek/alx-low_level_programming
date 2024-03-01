6bb356c73299b4f1b168edd270b2abba2d7480ac
donmaleek <bamboloshari@gmail.com>
2024-03-01 07:29:09 -0800

    0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+**_memset - fuction that fills memory with constant byte
+*@s: Memory area to be filled
+*@b: char to copy
+*@n: number of times to copy b(char to copy)
+*Return: Pointer to the memory area s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN.H_
+#define _MAIN.H_
+
+#include<stdi0.h>
+#include<stdlib.h>
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+#endif/*main_h*/ 
