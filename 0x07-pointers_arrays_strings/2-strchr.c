5fe609b0fe5e740123a5768213fa17c291413777
donmaleek <bamboloshari@gmail.com>
2024-03-04 00:22:47 -0800

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_strchr - locate a character in a string
+*@s: The string to be searched
+*@c: The character to be located
+*Return: if c is found, return pointer to it
+*if c is not found return -null
+*/
+char *_strchr(char *s, char c)
+{
+int index;
+
+for (index = 0; s[index] >= '\0'; index++)
+	{
+	if(s[index] == c)
+	return (s + index);
+	}
+	return ('\0');
+}
